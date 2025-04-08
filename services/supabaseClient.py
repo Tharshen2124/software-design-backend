68cf2d01434530524e9fd6b958147d1bba33e2a0
Janelle Tan <my.janelle.tan@gmail.com>
2025-04-09 03:49:15 +0800

    chore: update SUPABASE_KEY environment variable
    Updated the SUPABASE_KEY environment variable in
    the supabaseClient.py file to match the key
    in the env file the team uses.

--- services/supabaseClient.py
+++ services/supabaseClient.py
@@ -5,5 +5,5 @@ from supabase import create_client, Client
 load_dotenv()
 
 url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_ANON_KEY")
+key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
