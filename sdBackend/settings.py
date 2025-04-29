75c400120c72df2d899b5023a0e81dc0465f4a33
ParalysisCat <theonlyirfanismail5151@gmail.com>
2025-04-29 17:36:33 +0800

    Inclusion of Community Posts backend logic
    Community Posts is done in a different app called 'community_posts' rather than in custom_auth. Hence, a new folder of new files is pushed.

--- sdBackend/settings.py
+++ sdBackend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "corsheaders",
     'custom_auth',
+    'community_posts',
 ]
 
 MIDDLEWARE = [
--- sdBackend/urls.py
+++ sdBackend/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('custom_auth.urls')),
+    path("posts/", include("community_posts.urls")), 
 ]
