457df2b43aca242c94649621f6a93b0c8c554a72
ParalysisCat <theonlyirfanismail5151@gmail.com>
2025-05-02 23:01:16 +0800

    added view individual posts
    def post_detail(request, post_id): is added which views individual posts
    
    delete needs to be worked on cuz it doesnt filter by current user only

--- community_posts/urls.py
+++ community_posts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("posts/", views.posts_list_create),
+    path('posts/<uuid:post_id>/', views.post_detail),  # GET/DELETE
 ]
--- community_posts/views.py
+++ community_posts/views.py
@@ -1,6 +1,7 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 from django.views.decorators.csrf import csrf_exempt
 from supabase_config.supabase_client import supabase
+
 
 @csrf_exempt
 def posts_list_create(request):
@@ -15,3 +16,16 @@ def posts_list_create(request):
     
     res = supabase.table('posts').select('*').execute()
     return JsonResponse({'posts': res.data})
+
+@csrf_exempt
+def post_detail(request, post_id):
+    if request.method == 'GET':
+        # View single post
+        res = supabase.table('posts').select('*').eq('id', post_id).execute()
+        if not res.data:
+            raise Http404("Post not found")
+        return JsonResponse(res.data[0])
+    
+    elif request.method == 'DELETE':
+        supabase.table('posts').delete().eq('id', post_id).execute()
+        return JsonResponse({'status': 'deleted'}, status=204)
