483d64bb3426e308b65c2cb2b5d2ecea0f778a94
Tharshen2124 <tharshensurianbalan@gmail.com>
2025-05-02 22:47:40 +0800

    feat: include upload image feature to cloudinary

--- community_posts/views.py
+++ community_posts/views.py
@@ -1,17 +1,26 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from supabase_config.supabase_client import supabase
+from cloudinary_client import CloudinaryClient
 
 @csrf_exempt
 def posts_list_create(request):
     if request.method == 'POST':
+        image = request.FILES.get('image_url')
+        if image:
+            cloudinary_client = CloudinaryClient()
+            response = cloudinary_client.upload(image)
+        else:
+            return JsonResponse({"error": "No image provided"}, status=400)
+
         data = {
             'title': request.POST.get('title'),
             'content': request.POST.get('content'),
-            'user_id': request.POST.get('user_id')
+            'user_id': request.POST.get('user_id'),
+            'image_url': response['secure_url']
         }
+
         res = supabase.table('posts').insert(data).execute()
         return JsonResponse(res.data[0], status=201)
     
-    res = supabase.table('posts').select('*').execute()
-    return JsonResponse({'posts': res.data})
+    return JsonResponse({"error": "Invalid request method"}, status=405)
