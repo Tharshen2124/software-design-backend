8eca0ddc26aadd859e350c36ec73df7a9580d5c9
Tharshen2124 <tharshensurianbalan@gmail.com>
2025-06-10 17:33:58 +0800

    fix: add setImage to complaints director

---
+++ complaints/case_director.py
@@ -0,0 +1,24 @@
+from interfaces.complaint_builder_interface import CaseComponentBuilder
+from .adapters.complaint_adapter import ComplaintData
+from .adapters.maintenance_adapter import MaintenanceProjectData
+
+# builds the complaint based on the interface using the data provided 
+class CaseDirector:
+   def buildComplaints(self, builder: CaseComponentBuilder, data: ComplaintData):
+      builder.setHeader(data.title)
+      builder.setDetails(data.description)
+      builder.setCitizenID(data.citizen_id)
+      builder.setStatus(data.status)
+      builder.setImage(data.image_url)
+      
+
+      return builder.complaint
+   
+   def buildMaintenancePlan(self, builder: CaseComponentBuilder, data: MaintenanceProjectData):
+      builder.setHeader(data.title)
+      builder.setDetails(data.description)
+      builder.setStatus(data.status)
+      builder.setImage(data.image_url)
+      # builder.setFollowUp(data.follow_up)
+
+      return builder.maintenance_plan
