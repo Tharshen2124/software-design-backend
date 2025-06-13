605c5d597f4d0de83261269c20edc177d406cd87
Tharshen2124 <tharshensurianbalan@gmail.com>
2025-06-13 19:37:10 +0800

    feat: create logic for maintenance projects and modify complaints logic

--- complaints/urls/maintenance_urls.py
+++ complaints/urls/maintenance_urls.py
@@ -3,9 +3,7 @@ from complaints.views import maintenance_views
 
 urlpatterns = [
     path("create/", maintenance_views.create),
-    # path("custom-admin/", maintenance_views.get_all_maintenance_plans),
-    # path("custom-admin/<int:maintenance_id>/", maintenance_views.get_maintenance_plan),
-    # path("government/", maintenance_views.get_all_filtered_maintenance_plans),
-    # path("government/<int:maintenance_id>/", maintenance_views.get_filtered_maintenance_plan),
-    # path("update-status/<int:maintenance_id>/", maintenance_views.update_maintenance_plan_status),
+    path("company/<str:maintenance_company_id>/", maintenance_views.get_all_maintenance_projects_for_company),
+    path("project/<str:project_id>/", maintenance_views.get_maintenance_projects_for_company),
+    path("update/", maintenance_views.update),
 ]
